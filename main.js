96f4895e15635fa86098be53ce851beaaec43cc2
Brian Cui on LUBUNTU-TWO <analytalica@gmail.com>
2015-07-14 21:00:05 -0500

    Implemented sending of emails from briancui address

--- main.js
+++ main.js
@@ -4,5 +4,27 @@ var request = require('request');
 console.log("Hello, world!");
 console.log(config.mandrillKey);
 
+request({ 'url': 'https://mandrillapp.com/api/1.0/messages/send.json',
+	  'method': 'POST',
+	  'json': { 'key': config.mandrillKey,
+		    'message': {
+			'from_email': config.email.from,
+			'to': [
+			    {
+				'email': config.email.to,
+				'type': 'to'
+			    }],
+			'autotext': 'true',
+			'subject': config.email.subject,
+			'html': config.email.body,
+		    }
+		  }
+	}, function(error, response, body) {
+	    console.log('error ' + JSON.stringify(error));
+	    console.log('response ' + JSON.stringify(response));
+	    console.log('body ' + JSON.stringify(body));
+	});
+
+	
 
 
